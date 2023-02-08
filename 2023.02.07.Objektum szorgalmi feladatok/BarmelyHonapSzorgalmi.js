0be9fbaa095993056b4f7c48bfa23826228303dd
IllesKati <illeskatalin@hotmail.com>
2023-02-08 21:03:58 +0100

    UNIO objektum szorgalmi

---
+++ 2023.02.07.Objektum szorgalmi feladatok/BarmelyHonapSzorgalmi.js
@@ -0,0 +1,142 @@
+<script>
+
+const EuropaiUnio = [{
+        orszag: "Ausztria",
+        csatlakozas: "1995.01.01"
+    },
+    {
+        orszag: "Belgium",
+        csatlakozas: "1958.01.01"
+    },
+    {
+        orszag: "Bulgária",
+        csatlakozas: "2007.01.01"
+    },
+    {
+        orszag: "Ciprus",
+        csatlakozas: "2004.05.01"
+    },
+    {
+        orszag: "Csehország",
+        csatlakozas: "2004.05.01"
+    },
+    {
+        orszag: "Dánia",
+        csatlakozas: "1973.01.01"
+    },
+    {
+        orszag: "Észtország",
+        csatlakozas: "2004.05.01"
+    },
+    {
+        orszag: "Finnország",
+        csatlakozas: "1995.01.01"
+    },
+    {
+        orszag: "Franciaország",
+        csatlakozas: "1958.01.01"
+    },
+    {
+        orszag: "Görögország",
+        csatlakozas: "1981.01.01"
+    },
+    {
+        orszag: "Hollandia",
+        csatlakozas: "1958.01.01"
+    },
+    {
+        orszag: "Horvátország",
+        csatlakozas: "2013.07.01"
+    },
+    {
+        orszag: "Írország",
+        csatlakozas: "1973.01.01"
+    },
+    {
+        orszag: "Lengyelország",
+        csatlakozas: "2004.05.01"
+    },
+    {
+        orszag: "Lettország",
+        csatlakozas: "2004.05.01"
+    },
+    {
+        orszag: "Litvánia",
+        csatlakozas: "2004.05.01"
+    },
+    {
+        orszag: "Luxemburg",
+        csatlakozas: "1958.01.01"
+    },
+    {
+        orszag: "Magyarország",
+        csatlakozas: "2004.05.01"
+    },
+    {
+        orszag: "Málta",
+        csatlakozas: "2004.05.01"
+    },
+    {
+        orszag: "Németország",
+        csatlakozas: "1958.01.01"
+    },
+    {
+        orszag: "Olaszország",
+        csatlakozas: "1958.01.01"
+    },
+    {
+        orszag: "Portugália",
+        csatlakozas: "1986.01.01"
+    },
+    {
+        orszag: "Románia",
+        csatlakozas: "2007.01.01"
+    },
+    {
+        orszag: "Spanyolország",
+        csatlakozas: "1986.01.01"
+    },
+    {
+        orszag: "Svédország",
+        csatlakozas: "1995.01.01"
+    },
+    {
+        orszag: "Szlovákia",
+        csatlakozas: "2004.05.01"
+    },
+    {
+        orszag: "Szlovénia",
+        csatlakozas: "2004.05.01"
+    }
+];
+
+//A csatlakozás hónapja bármilyen hónap lehet...
+
+let month=prompt("Add meg a hónapot:");
+function Csatlakozas()
+{
+	let csatlakozas=true;
+    for(let i=0;i<EuropaiUnio.length;i++){
+    	if(EuropaiUnio[i].csatlakozas.includes(month)){
+        	csatlakozas=true;
+                    	return true;
+        }
+    }
+    return false;
+    }
+
+
+function EredmenyKiiro (eredmeny,eredmenySzovegEgy, eredmenySzovegKetto){
+	if(eredmeny==true){
+		document.write("<br>"+eredmenySzovegEgy);
+	}
+    else{
+    	document.write("<br>"+eredmenySzovegKetto);
+    }
+}
+EredmenyKiiro(Csatlakozas(), "Ebben a hónapban történt csatlakozás.", "Ebben a hónapban nem történt csatlakozás.");
+
+
+
+</script>
+
