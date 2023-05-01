f2f1fbdf50e726c5853da4e2660fd302495f7393
IllesKati <illeskatalin@hotmail.com>
2023-05-01 13:28:41 +0200

    Tesztkérdések_szorgalmi

---
+++ 2023.05.01.Tesztkérdések_szorgalmi/app.js
@@ -0,0 +1,403 @@
+var quiz = {
+    "JS": [
+        {
+            "id": 1,
+            "question": "A felsorolt címsorok közül válaszd ki a legnagyobb méretet:",
+            "options": [
+                {
+                    "a": "&lt;h1&gt;",
+                    "b": "&lt;h2&gt;",
+                    "c": "&lt;h5&gt;",
+                    "d": "&lt;h6&gt;"
+                }
+            ],
+            "answer": "&lt;h1&gt;",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 2,
+            "question": "Minek a rövidítése a HTML?",
+            "options": [
+                {
+                    "a": "Hyperlink Marked Language",
+                    "b": "HyperText Markup Language",
+                    "c": "HyperText Marked Language",
+                    "d": "HyperLink Markup Language"
+                }
+            ],
+            "answer": "HyperText Markup Language",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 3,
+            "question": "Melyek a HTML dokuentum alapvető részei?",
+            "options": [
+                {
+                    "a": "&lt;html, xml, sxl&gt;",
+                    "b": "&lt;html, headings, body&gt;",
+                    "c": "&lt;html, head, body&gt;",
+                    "d": "&lt;html, head, title&gt;"
+                }
+            ],
+            "answer": "&lt;html, head, body&gt;",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 4,
+            "question": "Melyik a helyes beállítás, ha a szövegemet sorkizártra szeretném igazítani?",
+            "options": [
+                {
+                    "a": "text-transform",
+                    "b": "text-decoration",
+                    "c": "text-indent",
+                    "d": "text-align:justify"
+                }
+            ],
+            "answer": "text-align:justify",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 5,
+            "question": "A box-shadow stílussal milyen változtatást eszközölhetünk?",
+            "options": [
+                {
+                    "a": "Az adott szöveges elem köré árnyékot helyezünk.",
+                    "b": "Az adott elemet középre helyezhetjük.",
+                    "c": "Az adott div elem box modellje köré árnyékot helyezhetünk.",
+                    "d": "Az adott elemre magasságot állíthatunk be."
+                }
+            ],
+            "answer": "Az adott div elem box modellje köré árnyékot helyezhetünk.",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 6,
+            "question": "Melyik CSS utasítás a helyes, ha félkövér betűt szeretnénk?",
+            "options": [
+                {
+                    "a": "font-weight:bold",
+                    "b": "font-weight:italic",
+                    "c": "font-weight:big",
+                    "d": "font-weight:300"
+                }
+            ],
+            "answer": "font-weight:bold",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 7,
+            "question": "Hány oszlopos a Bootstrap grid rendszerének felosztása?",
+            "options": [
+                {
+                    "a": "5",
+                    "b": "12",
+                    "c": "8",
+                    "d": "2"
+                }
+            ],
+            "answer": "12",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 8,
+            "question": "Melyik osztállyal hozhatsz létre gombcsoportokat?",
+            "options": [
+                {
+                    "a": "btn-siblings",
+                    "b": "btn-style",
+                    "c": "btn-group",
+                    "d": "btn-append"
+                }
+            ],
+            "answer": "btn-group",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 9,
+            "question": "Melyik kulcsszóval NEM lehet változót létrehozni a JavaScript nyelvben?",
+            "options": [
+                {
+                    "a": "var",
+                    "b": "const",
+                    "c": "let",
+                    "d": "trim"
+                }
+            ],
+            "answer": "trim",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 10,
+            "question": "Melyik az a feltétel, aminek NEM kell teljesülnie egy mezőre, ami a felhasználó nevét tartalmazza?",
+            "options": [
+                {
+                    "a": "Tartalmazzon kis- és nagybetűket is.",
+                    "b": "Ne csak szóközökből álljon.",
+                    "c": "Tartalmaznia kell számot.",
+                    "d": "Ne legyen üres.",
+                }
+            ],
+            "answer": "Tartalmaznia kell számot.",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 11,
+            "question": "Mire szolgál a Git clone parancsa?",
+            "options": [
+                {
+                    "a": "Törli a fájlt.",
+                    "b": "Letölt egy távoli repository-t.",
+                    "c": "Feltölti a módosításokat.",
+                    "d": "Letölt egy távoli fájlt."
+                }
+            ],
+            "answer": "Letölt egy távoli repository-t.",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 12,
+            "question": "Melyik ingyenes verziókövető szerver szolgáltatást használtuk a tanfolyam során?",
+            "options": [
+                {
+                    "a": "NVS",
+                    "b": "GitHub",
+                    "c": "GitLub",
+                    "d": "Azure"
+                }
+            ],
+            "answer": "GitHub",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 13,
+            "question": "Melyik függvénynév helytelen?",
+            "options": [
+                {
+                    "a": "sk",
+                    "b": "tombGeneralo",
+                    "c": "getUserName",
+                    "d": "AtlagSzamitas"
+                }
+            ],
+            "answer": "sk",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 14,
+            "question": "Melyik a helyes változónév a Clean Code szabályai szerint?",
+            "options": [
+                {
+                    "a": "star_wars",
+                    "b": "k",
+                    "c": "_alma",
+                    "d": "pirosAlma"
+                }
+            ],
+            "answer": "pirosAlma",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 15,
+            "question": "A box-model-ben belülről kifelé haladva milyen részei vannak?",
+            "options": [
+                {
+                    "a": "content, padding, border, margin",
+                    "b": "padding, content, border, margin",
+                    "c": "margin, border, padding, content",
+                    "d": "padding, border, content, margin"
+                }
+            ],
+            "answer": "content, padding, border, margin",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 16,
+            "question": "Melyik CSS tulajdonság adja meg a betűtípust?",
+            "options": [
+                {
+                    "a": "font-type",
+                    "b": "font-size",
+                    "c": "font-style",
+                    "d": "font-family"
+                }
+            ],
+            "answer": "font-family",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 17,
+            "question": "Hogyan nevezzük az objektumokhoz tartozó függvényeket?",
+            "options": [
+                {
+                    "a": "metódus",
+                    "b": "függvény",
+                    "c": "ciklus",
+                    "d": "eljárás"
+                }
+            ],
+            "answer": "metódus",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 18,
+            "question": "A for...in ciklus az objektum elemeinek melyik részét járja be?",
+            "options": [
+                {
+                    "a": "kulcsokat",
+                    "b": "értékeket",
+                    "c": "indexeket",
+                    "d": "bejegyzéseket"
+                }
+            ],
+            "answer": "kulcsokat",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 19,
+            "question": "Milyen típusú lesz a b változó? var b=10;",
+            "options": [
+                {
+                    "a": "string",
+                    "b": "any",
+                    "c": "number",
+                    "d": "boolen"
+                }
+            ],
+            "answer": "number",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 20,
+            "question": "Mi a TypeScript és a JavaScript közötti kapcsolat?",
+            "options": [
+                {
+                    "a": "A TypeScript a JavaScript egy szuperkészlete.",
+                    "b": "A TypeScript pontosan ugyanaz, mint a JavaScript.",
+                    "c": "A TypeScript a JavaScript egy részhalmaza.",
+                    "d": "Nincs közöttük kapcsolat."
+                }
+            ],
+            "answer": "A TypeScript a JavaScript egy szuperkészlete.",
+            "score": 0,
+            "status": ""
+        },
+
+    ]
+}
+var quizApp = function () {
+    this.score = 0;
+    this.qno = 1;
+    this.currentque = 0;
+    var totalque = quiz.JS.length;
+    this.displayQuiz = function (cque) {
+        this.currentque = cque;
+        if (this.currentque < totalque) {
+            $("#tque").html(totalque);
+            $("#previous").attr("disabled", false);
+            $("#next").attr("disabled", false);
+            $("#qid").html(quiz.JS[this.currentque].id + '.');
+            $("#question").html(quiz.JS[this.currentque].question);
+            $("#question-options").html("");
+            for (var key in quiz.JS[this.currentque].options[0]) {
+                if (quiz.JS[this.currentque].options[0].hasOwnProperty(key)) {
+                    $("#question-options").append(
+                        "<div class='form-check option-block'>" +
+                        "<label class='form-check-label'>" +
+                        "<input type='radio' class='form-check-input' name='option' id='q" + key + "' value='" + quiz.JS[this.currentque].options[0][key] + "'><span id='optionval'>" +
+                        quiz.JS[this.currentque].options[0][key] +
+                        "</span></label>"
+                    );
+                }
+            }
+        }
+        if (this.currentque <= 0) {
+            $("#previous").attr("disabled", true);
+        }
+        if (this.currentque >= totalque) {
+            $('#next').attr('disabled', true);
+            for (var i = 0; i < totalque; i++) {
+                this.score = this.score + quiz.JS[i].score;
+            }
+            return this.showResult(this.score);
+        }
+    }
+    this.showResult = function (scr) {
+        $("#result").addClass('result');
+        $("#result").html("<h1 class='res-header'>Total Score: &nbsp;" + scr + '/' + totalque + "</h1>");
+        for (var j = 0; j < totalque; j++) {
+            var res;
+            if (quiz.JS[j].score == 0) {
+                res = '<span class="wrong">' + quiz.JS[j].score + '</span><i class="fa fa-remove c-wrong"></i>';
+            } else {
+                res = '<span class="correct">' + quiz.JS[j].score + '</span><i class="fa fa-check c-correct"></i>';
+            }
+            $("#result").append(
+                '<div class="result-question"><span>Q ' + quiz.JS[j].id + '</span> &nbsp;' + quiz.JS[j].question + '</div>' +
+                '<div><b>Correct answer:</b> &nbsp;' + quiz.JS[j].answer + '</div>' +
+                '<div class="last-row"><b>Score:</b> &nbsp;' + res +
+                '</div>'
+            );
+        }
+    }
+    this.checkAnswer = function (option) {
+        var answer = quiz.JS[this.currentque].answer;
+        option = option.replace(/</g, "&lt;") //for <
+        option = option.replace(/>/g, "&gt;") //for >
+        option = option.replace(/"/g, "&quot;")
+        if (option == quiz.JS[this.currentque].answer) {
+            if (quiz.JS[this.currentque].score == "") {
+                quiz.JS[this.currentque].score = 1;
+                quiz.JS[this.currentque].status = "correct";
+            }
+        } else {
+            quiz.JS[this.currentque].status = "wrong";
+        }
+    }
+    this.changeQuestion = function (cque) {
+        this.currentque = this.currentque + cque;
+        this.displayQuiz(this.currentque);
+    }
+}
+var jsq = new quizApp();
+var selectedopt;
+$(document).ready(function () {
+    jsq.displayQuiz(0);
+    $('#question-options').on('change', 'input[type=radio][name=option]', function (e) {
+        //var radio = $(this).find('input:radio');
+        $(this).prop("checked", true);
+        selectedopt = $(this).val();
+    });
+});
+$('#next').click(function (e) {
+    e.preventDefault();
+    if (selectedopt) {
+        jsq.checkAnswer(selectedopt);
+    }
+    jsq.changeQuestion(1);
+});
+$('#previous').click(function (e) {
+    e.preventDefault();
+    if (selectedopt) {
+        jsq.checkAnswer(selectedopt);
+    }
+    jsq.changeQuestion(-1);
+});
